ec8330228c023775269caef722d01b3978645a8d
Piotr Krzyminski <piotrkrzyminski96@gmail.com>
2018-10-07 12:02:18 +0200

    Initial commit

---
+++ edziennik-front/src/main/java/pl/dziennik/WebApplication.java
@@ -0,0 +1,15 @@
+package pl.dziennik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring application main class.
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
