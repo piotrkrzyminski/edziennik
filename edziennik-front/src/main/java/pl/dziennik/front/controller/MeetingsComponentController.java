2360011a40a890422ad4d4fe6ed9c9f31b54b21e
Piotr Krzyminski <piotr.krzyminski96@gmail.com>
2018-12-02 12:41:37 +0100

    Fixed meetings table

--- edziennik-front/src/main/java/pl/dziennik/front/controller/MeetingsComponentController.java
+++ edziennik-front/src/main/java/pl/dziennik/front/controller/MeetingsComponentController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.dziennik.core.services.user.StudentService;
 import pl.dziennik.facades.MeetingFacade;
+import pl.dziennik.facades.data.MeetingData;
+import pl.dziennik.facades.data.ScheduleData;
+import pl.dziennik.facades.data.ScheduleDayData;
 import pl.dziennik.model.user.ClassModel;
 import pl.dziennik.model.user.StudentModel;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Controller for meetings fragment.
@@ -50,20 +53,4 @@ public class MeetingsComponentController extends PageController {
             }
         }
     }
-
-    /*private void prepareMeetingData(final Model model) {
-        final String userEmail = currentUserName(model);
-        if(userEmail == null) {
-            return;
-        }
-
-        StudentModel student = studentService.getStudentByEmail(userEmail);
-        if(student != null) {
-            final ClassModel classModel = studentService.getClassByStudentEmail(student.getEmail());
-            if(classModel != null) {
-                model.addAttribute("meetings", meetingFacade.getMeetingsForClass(classModel.getName()));
-                model.addAttribute("startHours", Arrays.asList("08:00", "09:00", "10:00", "11:00", "12:00", "01:00", "02:00"));
-            }
-        }
-    }*/
 }
