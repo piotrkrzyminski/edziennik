76f65ed909cf383990e2732413d2e97ad31ea3d9
Adrian Krysiak <adi.krysiak@gmail.com>
2018-12-16 17:57:55 +0100

    Merge od Justyny, zmiany w dodawaniu ocen

--- edziennik-front/src/main/resources/static/js/justTest.js
+++ edziennik-front/src/main/resources/static/js/justTest.js
@@ -1,34 +1,22 @@
 function openMeetingDetails(day, id)
 {
-    console.log('Ohooo ' + id);
-    console.log('Ohooo ' + day);
-    //'/e-dziennik/meetings/details?date=' + ${#dates.format(weekDates[j-1], 'dd.MM.yyyy')} + '&' + 'id=' + ${meetings[i-1][j-1].id}
     var url = "/e-dziennik/meetings/details?date=" + day + "&" + "id=" + id;
     console.log(url);
     loadComponentByUrl(url);
-    console.log('ttttttt');
 }
 
 function openPresence(day, id)
 {
-    console.log('Ohooo ' + id);
-    console.log('Ohooo ' + day);
-    //'/e-dziennik/meetings/details?date=' + ${#dates.format(weekDates[j-1], 'dd.MM.yyyy')} + '&' + 'id=' + ${meetings[i-1][j-1].id}
     var url = "/e-dziennik/meetings/presence?date=" + day + "&" + "id=" + id;
     console.log(url);
     loadInnerComponentByUrl(url);
-    console.log('ttttttt');
 }
 
 function openAddGrade(day, id)
 {
-    console.log('Ohooo ' + id);
-    console.log('Ohooo ' + day);
-    //'/e-dziennik/meetings/details?date=' + ${#dates.format(weekDates[j-1], 'dd.MM.yyyy')} + '&' + 'id=' + ${meetings[i-1][j-1].id}
     var url = "/e-dziennik/grades/add?date=" + day + "&" + "id=" + id;
     console.log(url);
     loadInnerComponentByUrl(url);
-    console.log('ttttttt');
 }
 
 function returnToMeetings()
@@ -57,27 +45,3 @@ function prevWeek(date)
     var url = "/e-dziennik/meetings?date="+ dateResult.getDate() + "-" + (dateResult.getMonth()+1) + "-" + dateResult.getFullYear();
     loadComponentByUrl(url);
 }
-
-
-$('input.cb-value').prop("checked", true);
-$('.cb-value').click(function() {
-    console.log('Im here');
-    var mainParent = $(this).parent('.toggle-btn');
-    if($(mainParent).find('input.cb-value').is(':checked')) {
-        $(mainParent).addClass('active');
-    } else {
-        $(mainParent).removeClass('active');
-    }
-
-});
-
-$(".cb-value").click(function() {
-    console.log('Im here2');
-    var mainParent = $(this).parent('.toggle-btn');
-    if($(mainParent).find('input.cb-value').is(':checked')) {
-        $(mainParent).addClass('active');
-    } else {
-        $(mainParent).removeClass('active');
-    }
-
-});
